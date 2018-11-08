d662baa99e9291fb1837ca1012e7f8f977efe8e7
Malorie Lara <malorielara@users.noreply.github.com>
2018-11-08 17:45:35 -0600

    final

--- bamazon.sql
+++ bamazon.sql
@@ -11,16 +11,14 @@ CREATE TABLE Products(
     primary key (ItemId)
 );
 
-
--- into products
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Camelbak Pack', 'Hiking', 69.99, 100);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('AirPods', 'Electronics', 159.99, 200);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Metal Straws', 'Kitchenware', 9.99, 500);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Playstation4', 'Gaming', 499.99, 200);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Dog Sweater', 'Dog Clothing', 19.99, 150);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Spiderman Web Shooter','Toys', 25.99, 200);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Lady Bug Pillow Pet', 'Pillows', 9.99, 200);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Doritos', 'Food', 5.49, 175);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Jurrassic World', 'Movies', 29.99, 200);
--- INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Vans Slip-on Shoes','Shoes', 49.99, 100);
---
+into products
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Camelbak Pack', 'Hiking', 69.99, 100);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('AirPods', 'Electronics', 159.99, 200);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Metal Straws', 'Kitchenware', 9.99, 500);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Playstation4', 'Gaming', 499.99, 200);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Dog Sweater', 'Dog Clothing', 19.99, 150);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Spiderman Web Shooter','Toys', 25.99, 200);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Lady Bug Pillow Pet', 'Pillows', 9.99, 200);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Doritos', 'Food', 5.49, 175);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Jurrassic World', 'Movies', 29.99, 200);
+INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Vans Slip-on Shoes','Shoes', 49.99, 100);
